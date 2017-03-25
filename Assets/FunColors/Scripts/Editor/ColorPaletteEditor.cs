a70889f41b0ea52855a6d232c8e8c5c5505a5971
locura.madness@gmail.com <locura.madness@gmail.com>
2017-03-25 19:24:50 -0400

    Made the palette manager static (instead of being a singleton that gets instantiated) Tried to make Graphics update in edit mode (and failed) Set the name on the red/green palettes

--- Assets/FunColors/Scripts/Editor/ColorPaletteEditor.cs
+++ Assets/FunColors/Scripts/Editor/ColorPaletteEditor.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.SceneManagement;
+﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace FunColors
 {
